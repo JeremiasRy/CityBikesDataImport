20056fcd7590944d44a2b97438733fca39135a75
JeremiasRy <ryttari.jeremias@gmail.com>
2022-10-16 12:45:38 +0300

    Typo fix

--- CityBikeDataBase/dbo/stored procedures/Sp_Top5DepToRtn.sql
+++ CityBikeDataBase/dbo/stored procedures/Sp_Top5DepToRtn.sql
@@ -1,17 +1,17 @@
 ﻿-- =============================================
 -- Author:		Jeremias Ryttäri
 -- Create date: 16.10.2022
--- Description:	Stations top 5 departures that returned.
+-- Description:	Stations top 5 departure stations that returned to parameter station.
 -- =============================================
-CREATE PROCEDURE [dbo].[Sp_GetStationTop5DeparturesThatReturned]
+CREATE PROCEDURE [dbo].[Sp_Top5DepToRtn]
 	@StationId char(3)
 AS
 BEGIN
-	SELECT TOP (5) DepartureStationId as StationId, COUNT(*) as ReturnsCount
+	SELECT TOP (5) DepartureStationId as StationId, COUNT(*) as Amount
 	FROM journeys
 	WHERE ReturnStationId = @StationId
 	GROUP BY ReturnStationId, DepartureStationId
-	ORDER BY ReturnsCount DESC
+	ORDER BY Amount DESC
 END
 GO
 
--- CityBikeDataBase/dbo/stored procedures/Sp_Top5RtnToDep.sql
+++ CityBikeDataBase/dbo/stored procedures/Sp_Top5RtnToDep.sql
@@ -1,17 +1,17 @@
 ﻿-- =============================================
 -- Author:		Jeremias Ryttäri
 -- Create date: 16.10.2022
--- Description:	Stations top 5 departures that returned.
+-- Description:	Stations top 5 return stations from parameter station
 -- =============================================
-CREATE PROCEDURE [dbo].[Sp_GetStationTop5ReturnsThatDeparted]
+CREATE PROCEDURE [dbo].[Sp_Top5RtnToDep]
 	@StationId char(3)
 AS
 BEGIN
-	SELECT TOP (5) ReturnStationId as StationId, COUNT(*) as ReturnsCount
+	SELECT TOP (5) ReturnStationId as StationId, COUNT(*) as Amount
 	FROM journeys
 	WHERE DepartureStationId = @StationId
 	GROUP BY ReturnStationId, DepartureStationId
-	ORDER BY ReturnsCount DESC
+	ORDER BY Amount DESC
 END
 GO
 
