a7aab58d8102f74c87a57ba6da86e06e4487c150
JeremiasRy <ryttari.jeremias@gmail.com>
2022-10-23 11:27:26 +0300

    Filter station stats by month

--- CityBikeDataBase/dbo/stored procedures/Sp_GetStationStatistics.sql
+++ CityBikeDataBase/dbo/stored procedures/Sp_GetStationStatistics.sql
@@ -6,32 +6,33 @@
 -- Description:	Get statistics from journeys
 -- =============================================
 CREATE PROCEDURE [dbo].[Sp_GetStationStatistics] 
-	@StationId char(3)
+	@StationId char(3),
+	@Month int = null
 AS
 BEGIN
 		SELECT Count(Id) as Amount, 'Departures' as Type
 		FROM journeys
-		WHERE @StationId = DepartureStationId 
+		WHERE @StationId = DepartureStationId and (1=(CASE when @Month is null then 1 else 0 end) or MONTH(DepartureDate) = @Month)
 	UNION
 		SELECT Count(Id) as Amount, 'Returns' as Type
 		FROM journeys
-		WHERE @StationId = ReturnStationId
+		WHERE @StationId = ReturnStationId and (1=(CASE when @Month is null then 1 else 0 end) or MONTH(DepartureDate) = @Month)
 	UNION
 		SELECT AVG(Duration) as Amount, 'Duration average departures (s)' as Type
 		FROM journeys
-		WHERE @StationId = DepartureStationId
+		WHERE @StationId = DepartureStationId and (1=(CASE when @Month is null then 1 else 0 end) or MONTH(DepartureDate) = @Month)
 	UNION
 		SELECT AVG(Duration) as Amount, 'Duration average returns (s)' as Type
 		FROM journeys
-		WHERE @StationId = ReturnStationId
+		WHERE @StationId = ReturnStationId and (1=(CASE when @Month is null then 1 else 0 end) or MONTH(DepartureDate) = @Month)
 	UNION
 		SELECT AVG(Distance) as Amount, 'Distance average departures (m)' as Type
 		FROM journeys
-		WHERE @StationId = DepartureStationId
+		WHERE @StationId = DepartureStationId and (1=(CASE when @Month is null then 1 else 0 end) or MONTH(DepartureDate) = @Month)
 			UNION
 		SELECT AVG(Distance) as Amount, 'Distance average returns (m)' as Type
 		FROM journeys
-		WHERE @StationId = ReturnStationId
+		WHERE @StationId = ReturnStationId and (1=(CASE when @Month is null then 1 else 0 end) or MONTH(DepartureDate) = @Month)
 END
 GO
 
