259b360a44a721587819f029e843b638f2607d00
JeremiasRy <ryttari.jeremias@gmail.com>
2022-10-24 09:55:32 +0300

    Delete procedures

---
+++ CityBikeDataBase/dbo/stored procedures/Sp_DeleteJourney.sql
@@ -0,0 +1,5 @@
+﻿CREATE PROCEDURE [dbo].[Sp_DeleteJourney]
+	@Id int
+AS
+ 	DELETE from journeys Where Id = @Id
+RETURN 0
---
+++ CityBikeDataBase/dbo/stored procedures/Sp_DeleteStation.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[Sp_DeleteStation]
+	@stationId char(3)
+AS	
+	ALTER TABLE journeys
+	NOCHECK CONSTRAINT FK_DepStationId, FK_RetStationId
+	DELETE from stations where StationId = @stationId
+	ALTER TABLE journeys
+	WITH CHECK 
+	CHECK CONSTRAINT FK_DepSTationId, FK_RetSTationId
+RETURN 0
